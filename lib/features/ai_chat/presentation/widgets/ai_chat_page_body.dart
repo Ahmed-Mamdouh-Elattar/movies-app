6218959c843b0ff392f05beaad57db1c11adf9db
Ahmed-Mamdouh-Elattar <Ahmed.Mamdouh.Elattar@gmail.com>
2025-11-05 20:38:09 +0200

    create simple ui for ai chat page

--- lib/features/ai_chat/presentation/widgets/ai_chat_page_body.dart
+++ lib/features/ai_chat/presentation/widgets/ai_chat_page_body.dart
@@ -1,10 +1,36 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:movies_app/core/config/app_color.dart';
 
 class AiChatPageBody extends StatelessWidget {
   const AiChatPageBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView();
+    return Column(
+      children: [
+        AppBar(title: const Text("Movies Chat Bot")),
+        const Expanded(child: SizedBox()),
+        Row(
+          children: [
+            const SizedBox(width: 8),
+            Expanded(
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(24),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(width: 8),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.send, color: AppColor.blue),
+            ),
+          ],
+        ),
+        const SizedBox(height: 16),
+      ],
+    );
   }
 }
