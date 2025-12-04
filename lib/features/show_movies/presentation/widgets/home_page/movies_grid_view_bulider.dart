cfeba72f759ad52d19717234dde2f64a6e96c1b9
Ahmed-Mamdouh-Elattar <Ahmed.Mamdouh.Elattar@gmail.com>
2025-12-04 19:08:10 +0200

    refactor code to use hook widget instead of stateful wdget

--- lib/features/show_movies/presentation/widgets/home_page/movies_grid_view_bulider.dart
+++ lib/features/show_movies/presentation/widgets/home_page/movies_grid_view_bulider.dart
@@ -1,31 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:movies_app/core/enums/movie_category_enum.dart';
 
 import 'package:movies_app/core/networking/network_info/network_cubit/network_cubit.dart';
 import 'package:movies_app/features/show_movies/presentation/managers/movies_category_cubit/movies_category_cubit.dart';
 import 'package:movies_app/features/show_movies/presentation/widgets/home_page/movie_grid_view.dart';
 
-class MoviesGridViewBuilder extends StatefulWidget {
+class MoviesGridViewBuilder extends HookWidget {
   const MoviesGridViewBuilder({required this.movieCategory, super.key});
   final MovieCategory movieCategory;
-
-  @override
-  State<MoviesGridViewBuilder> createState() => _MoviesGridViewBuilderState();
-}
-
-class _MoviesGridViewBuilderState extends State<MoviesGridViewBuilder> {
-  @override
-  void initState() {
-    context.read<MoviesCategoryCubit>().reset();
-    context.read<MoviesCategoryCubit>().getMoviesByCategory(
-      category: widget.movieCategory.value,
-    );
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
+    useEffect(() {
+      context.read<MoviesCategoryCubit>().reset();
+      context.read<MoviesCategoryCubit>().getMoviesByCategory(
+        category: movieCategory.value,
+      );
+      return null;
+    }, []);
     return BlocListener<NetworkCubit, NetworkState>(
       listenWhen: (previous, current) {
         if (previous is NetworkDisconnected && current is NetWorkConnected) {
@@ -37,7 +30,7 @@ class _MoviesGridViewBuilderState extends State<MoviesGridViewBuilder> {
       listener: (context, state) {
         if (state is NetWorkConnected) {
           context.read<MoviesCategoryCubit>().getMoviesByCategory(
-            category: widget.movieCategory.value,
+            category: movieCategory.value,
           );
         }
       },
