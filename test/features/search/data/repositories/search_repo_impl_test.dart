098ca89216b34919771f309028d0a8a774b1b765
Ahmed-Mamdouh-Elattar <Ahmed.Mamdouh.Elattar@gmail.com>
2025-12-18 22:25:07 +0200

    refactor search_repo_impl_test to test the right function (searchMovies())

--- test/features/search/data/repositories/search_repo_impl_test.dart
+++ test/features/search/data/repositories/search_repo_impl_test.dart
@@ -2,8 +2,12 @@ import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
+import 'package:movies_app/core/errors/server_failure.dart';
 import 'package:movies_app/core/models/movies_model/movies_model.dart';
+import 'package:movies_app/core/utils/response_result.dart';
 import 'package:movies_app/features/search/data/datasources/search_data_source.dart';
+import 'package:movies_app/features/search/data/repositories/search_repo_impl.dart';
+import 'package:movies_app/features/search/domain/entities/search_movies_entity.dart';
 
 import 'search_repo_impl_test.mocks.dart';
 
@@ -12,59 +16,68 @@ void main() {
   const page = 1;
   const query = "query";
   final cancelToken = CancelToken();
-  late SearchDataSource apiService;
+  late SearchDataSource searchDataSource;
+  late SearchRepoImpl searchRepoImpl;
   setUp(() {
-    apiService = MockSearchDataSource();
+    searchDataSource = MockSearchDataSource();
+    searchRepoImpl = SearchRepoImpl(searchDataSource, () => cancelToken);
   });
 
   group("test search repo impl", () {
     group("searchMovies()", () {
       test(
-        "should return movies model when call to search data source is successful",
+        "should return ResponseResult<List<SearchMoviesEntity>> when call to search data source is successful",
         () async {
           //arrange
-          final expectedMoviesModel = MoviesModel(
+          final moviesModel = MoviesModel(
             page: page,
             results: [],
             totalPages: 1,
             totalResults: 1,
           );
           when(
-            apiService.searchMovies(
+            searchDataSource.searchMovies(
               query: query,
               page: page,
               cancelToken: cancelToken,
             ),
-          ).thenAnswer((_) async => expectedMoviesModel);
+          ).thenAnswer((_) async => moviesModel);
           //act
-          final result = await apiService.searchMovies(
+          final result = await searchRepoImpl.searchMovies(
             query: query,
             page: page,
-            cancelToken: cancelToken,
           );
           //assert
-          expect(result, expectedMoviesModel);
+          expect(result, isA<ResponseResult<List<SearchMoviesEntity>>>());
         },
       );
       test(
-        "should throw exception when call to search data source fails",
+        "should return ResponseResult<List<SearchMoviesEntity>> when call to search data source fails",
         () async {
           //arrange
+          final tFailure = DioException(
+            requestOptions: RequestOptions(),
+            message: "message",
+            type: DioExceptionType.connectionError,
+          );
+          final expectedValue =
+              ResponseResult<List<SearchMoviesEntity>>.failure(
+                ServerFailure.fromDioError(tFailure),
+              );
           when(
-            apiService.searchMovies(
+            searchDataSource.searchMovies(
               query: query,
               page: page,
               cancelToken: cancelToken,
             ),
-          ).thenThrow(Exception());
+          ).thenAnswer((_) async => throw tFailure);
           //act
-          Future<MoviesModel> result() async => await apiService.searchMovies(
+          final result = await searchRepoImpl.searchMovies(
             query: query,
             page: page,
-            cancelToken: cancelToken,
           );
           //assert
-          expect(result(), throwsException);
+          expect(result, expectedValue);
         },
       );
     });
